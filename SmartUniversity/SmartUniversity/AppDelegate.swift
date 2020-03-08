48b49183ab5efe47b8599b7a8091a93dc667b8c7
Tom Skypala <xskypala@gmail.com>
2020-03-08 22:07:18 +0100

    feat: make sceneconfiguration generic
    Enables generic use of DependencyProviders by introducing SceneConfiguration generic class.

--- SmartUniversity/SmartUniversity/AppDelegate.swift
+++ SmartUniversity/SmartUniversity/AppDelegate.swift
@@ -28,7 +28,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     ) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
-        return ARSceneConfiguration(sceneSession: connectingSceneSession)
+        return SceneConfiguration<ARSceneDependencyProvider>(sceneSession: connectingSceneSession)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {

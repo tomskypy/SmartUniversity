26933269915e520c7edb64d6ea20325c1ed87dd8
Tom Skypala <xskypala@gmail.com>
2020-03-08 22:08:31 +0100

    rf: remove unused code from appdelegate

--- SmartUniversity/SmartUniversity/AppDelegate.swift
+++ SmartUniversity/SmartUniversity/AppDelegate.swift
@@ -31,11 +31,4 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         return SceneConfiguration<ARSceneDependencyProvider>(sceneSession: connectingSceneSession)
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after
-        // application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
 }

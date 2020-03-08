f98752a057d752154f8d36658f933c0201b34191
Tom Skypala <xskypala@gmail.com>
2020-03-08 20:32:30 +0100

    feat: introduce basescenedelegate
    Introduces BaseSceneDelegate to make SceneDelegate more testable.

--- SmartUniversity/SmartUniversity/Models/SceneDelegate.swift
+++ SmartUniversity/SmartUniversity/Models/SceneDelegate.swift
@@ -8,21 +8,13 @@
 
 import UIKit
 
-final class SceneDelegate<DependencyProvider: SceneDependencyProviding>: UIResponder, UIWindowSceneDelegate {
+final class SceneDelegate<DependencyProvider: SceneDependencyProviding>: BaseSceneDelegate {
 
     let dependencyProvider = DependencyProvider()
 
-    var window: UIWindow?
+    override func windowDidLoad(_ window: UIWindow) {
+        super.windowDidLoad(window)
 
-    func scene(
-        _ scene: UIScene,
-        willConnectTo session: UISceneSession,
-        options connectionOptions: UIScene.ConnectionOptions
-    ) {
-        guard let windowScene = scene as? UIWindowScene else { return }
-
-        let window = UIWindow(windowScene: windowScene)
-        self.window = window
         window.rootViewController = dependencyProvider.makeRootViewController()
 
         dependencyProvider.sceneHandler?.windowWillBecomeVisible(window)

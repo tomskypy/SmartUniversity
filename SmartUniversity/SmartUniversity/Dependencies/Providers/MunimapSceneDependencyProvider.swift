f3a99c9f422e1390869803798e8995a5813aab36
Tom Skypala <xskypala@gmail.com>
2020-03-11 03:00:02 +0100

    feat: add munimap scene dependency provider
    Adds munimap scene dependency provider. Additionally enables arbitrary loads in app's Info.plist to enable URL loading in the webview.

---
+++ SmartUniversity/SmartUniversity/Dependencies/Providers/MunimapSceneDependencyProvider.swift
@@ -0,0 +1,25 @@
+//
+//  MunimapSceneDependencyProvider.swift
+//  SmartUniversity
+//
+//  Created by Tomas Skypala on 11/03/2020.
+//  Copyright © 2020 Tomas Skypala. All rights reserved.
+//
+
+import UIKit
+
+final class MunimapSceneDependencyProvider: SceneDependencyProviding {
+
+    var sceneHandler: WindowSceneHandling?
+
+    private lazy var munimapServerURL: URL = {
+        guard let url = URL(string: "08668375-551f-4033-a3b8-f1f6715dfb79.htmlpasta.com/") else {
+            fatalError("Failed to create munimap server url.")
+        }
+        return url
+    }()
+
+    func makeRootViewController() -> UIViewController {
+        return MunimapViewController(munimapServerURL: munimapServerURL, webViewHandler: WebViewHandler.shared)
+    }
+}

442c1a587e8ee9fc259f2c146085e3b3b7bc5d31
Tom Skypala <xskypala@gmail.com>
2020-03-30 01:21:47 +0200

    feat: add qrpoint data model
    Adds QRPoint and ARLocalizedObject model structures.

---
+++ SmartUniversity/SmartUniversity/Models/Remote/QRPoint.swift
@@ -0,0 +1,25 @@
+//
+//  QRPoint.swift
+//  SmartUniversity
+//
+//  Created by Tomas Skypala on 29/03/2020.
+//  Copyright © 2020 Tomas Skypala. All rights reserved.
+//
+
+import Foundation
+
+struct QRPoint: Decodable {
+
+    let uuidString: String
+    let label: String
+    let muniMapPlaceID: String
+
+    let rooms: [ARLocalizedObject]
+
+    private enum CodingKeys : String, CodingKey {
+        case uuidString = "id"
+        case label
+        case muniMapPlaceID = "munimap"
+        case rooms = "arObjects"
+    }
+}

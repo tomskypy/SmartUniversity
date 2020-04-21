767a9e61defaf3d08d049ce667ba7e58b12beaa2
Tom Skypala <xskypala@gmail.com>
2020-04-22 00:39:21 +0200

    test: add presentationhandler tests

---
+++ SmartUniversity/SmartUniversityTests/Dependencies Tests/PresentationHandlerTests.swift
@@ -0,0 +1,38 @@
+//
+//  PresentationHandlerTests.swift
+//  SmartUniversityTests
+//
+//  Created by Tomas Skypala on 22/04/2020.
+//  Copyright © 2020 Tomas Skypala. All rights reserved.
+//
+
+import XCTest
+
+private final class TestableViewController: UIViewController {
+
+    var viewControllerToPresentReceivedInPresent: UIViewController?
+
+    override func present(_ viewControllerToPresent: UIViewController, animated flag: Bool, completion: (() -> Void)? = nil) {
+        viewControllerToPresentReceivedInPresent = viewControllerToPresent
+    }
+}
+
+final class PresentationHandlerTests: XCTestCase {
+
+    func testPresentDoesPresentViewControllerToPresentOnProvidedViewController() {
+        let presentingViewController = TestableViewController()
+        let expectedViewControllerToPresent = UIViewController()
+
+        PresentationHandler.shared.present(
+            expectedViewControllerToPresent,
+            onViewController: presentingViewController,
+            animated: true
+        )
+
+        XCTAssertEqual(
+            expectedViewControllerToPresent,
+            presentingViewController.viewControllerToPresentReceivedInPresent
+        )
+    }
+
+}

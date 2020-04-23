bf319b8a7c567e857c5d345c1c769301b3a894d8
Tom Skypala <xskypala@gmail.com>
2020-04-23 02:47:49 +0200

    feat: add arsceneviewhandling abstraction
    Adds ARSceneViewHandling and the implementation ARSceneViewHandler, enabling separated and convenient ARSCNView lifecycle handling.

---
+++ SmartUniversity/SmartUniversity/Dependencies/Handlers/ARSceneViewHandler.swift
@@ -0,0 +1,70 @@
+//
+//  ARSceneViewHandler.swift
+//  SmartUniversity
+//
+//  Created by Tomas Skypala on 23/04/2020.
+//  Copyright © 2020 Tomas Skypala. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+protocol ARSceneViewHandling {
+
+    var referenceImages: Set<ARReferenceImage> { get set }
+
+    func handleViewDidLoad(_ view: UIView)
+    func handleViewWillAppear(_ view: UIView)
+    func handleViewWillDisappear(_ view: UIView)
+}
+
+final class ARSceneViewHandler: ARSceneViewHandling {
+
+    var referenceImages: Set<ARReferenceImage> {
+        didSet {
+            setupAndRunSceneSession()
+        }
+    }
+
+    private var sceneView: ARSCNView?
+
+    private let sessionRunOptions: ARSession.RunOptions
+
+    init(
+        referenceImages: Set<ARReferenceImage> = Set(),
+        sessionRunOptions: ARSession.RunOptions = [.resetTracking, .removeExistingAnchors]
+    ) {
+        self.referenceImages = referenceImages
+        self.sessionRunOptions = sessionRunOptions
+    }
+
+    func handleViewDidLoad(_ view: UIView) {
+        guard var sceneContainerView = view as? ARSceneContainerView else { return }
+
+        let sceneView = ARSCNView()
+        sceneContainerView.arSceneView = sceneView
+        self.sceneView = sceneView
+    }
+
+    func handleViewWillAppear(_ view: UIView) {
+        setupAndRunSceneSession()
+    }
+
+    func handleViewWillDisappear(_ view: UIView) {
+        pauseSceneSession()
+    }
+
+    private func setupAndRunSceneSession() {
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.detectionImages = referenceImages
+
+        if ARWorldTrackingConfiguration.supportsFrameSemantics(.personSegmentationWithDepth) {
+            configuration.frameSemantics.insert(.personSegmentationWithDepth)
+        }
+        sceneView?.session.run(configuration, options: sessionRunOptions)
+    }
+
+    private func pauseSceneSession() {
+        sceneView?.session.pause()
+    }
+}

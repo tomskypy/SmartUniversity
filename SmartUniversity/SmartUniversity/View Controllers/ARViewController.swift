9152d960c4645ab23212faa2494fad9c31d09789
Tom Skypala <xskypala@gmail.com>
2020-05-01 20:52:08 +0200

    feat: add ar room visualization to ar vc

--- SmartUniversity/SmartUniversity/View Controllers/ARViewController.swift
+++ SmartUniversity/SmartUniversity/View Controllers/ARViewController.swift
@@ -13,20 +13,41 @@ final class ARViewController: BaseViewController<ARScreenView> {
     private var sceneViewHandler: ARSceneViewHandling
     private let sceneObjectProvider: SceneObjectProviding
 
+    private var roomsData: [ARLocalizedObjectData] = []
+
     convenience init() {
         let arReferenceImages = PosterReferenceImageProvider.shared.referenceImages
         let posterImage = PosterReferenceImageProvider.shared.image
         self.init(
             sceneViewHandler: ARSceneViewHandler(referenceImages: arReferenceImages),
             sceneObjectProvider: RoomsSceneObjectProvider(
-                model: RoomsSceneObjectProvider.Model(posterImage: posterImage, defaultTint: UIColor.red) // FIXME: make a dependency
-            )
+                model: .init(posterImage: posterImage, defaultTint: UIColor.systemPink) // FIXME: make a dependency for tint
+            ),
+            roomsData: [ // FIXME: Replace with actual data/provider
+                ARLocalizedObjectData(
+                    label: "test1",
+                    dimensions: .init(width: 2, height: 2, length: 2),
+                    position: .init(right: 0, up: 0, front: 10),
+                    tint: "#"
+                ),
+                ARLocalizedObjectData(
+                    label: "test2",
+                    dimensions: .init(width: 1, height: 1, length: 1),
+                    position: .init(right: 0, up: 3, front: 10),
+                    tint: "#008080FF"
+                )
+            ]
         )
     }
 
-    init(sceneViewHandler: ARSceneViewHandling, sceneObjectProvider: SceneObjectProviding) {
+    init(
+        sceneViewHandler: ARSceneViewHandling,
+        sceneObjectProvider: SceneObjectProviding,
+        roomsData: [ARLocalizedObjectData]
+    ) {
         self.sceneViewHandler = sceneViewHandler
         self.sceneObjectProvider = sceneObjectProvider
+        self.roomsData = roomsData
         super.init(nibName: nil, bundle: nil)
 
         self.sceneViewHandler.delegate = self
@@ -65,7 +86,11 @@ extension ARViewController: ARSceneViewHandlerDelegate {
         let posterNode = sceneObjectProvider.makeNodeFor(
             .poster(physicalSize: imageAnchor.referenceImage.physicalSize)
         )
-
         node.addChildNode(posterNode)
+
+        roomsData.forEach { roomData in
+            let roomNode = sceneObjectProvider.makeNodeFor(.room(objectData: roomData))
+            node.addChildNode(roomNode)
+        }
     }
 }

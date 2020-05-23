10c60b06513d2c3d7997ba40f5b1cf49e99d3158
Tom Skypala <xskypala@gmail.com>
2020-05-23 23:49:15 +0200

    feat: introduce basecoordinator
    Introduces BaseCoordinator and NavigationController abstractions.

---
+++ SmartUniversity/SmartUniversity/Coordinators/BaseCoordinator.swift
@@ -0,0 +1,26 @@
+//
+//  BaseCoordinator.swift
+//  SmartUniversity
+//
+//  Created by Tomas Skypala on 23/05/2020.
+//  Copyright © 2020 Tomas Skypala. All rights reserved.
+//
+
+import UIKit
+
+protocol NavigationController {
+
+    func pushViewController(_ viewController: UIViewController)
+}
+
+protocol BaseCoordinator {
+
+    init(navigationController: NavigationController)
+}
+
+extension UINavigationController: NavigationController {
+
+    func pushViewController(_ viewController: UIViewController) {
+        pushViewController(viewController, animated: true)
+    }
+}

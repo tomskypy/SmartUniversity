6f926d99679b9c5f12a11b83efec13e0741ea84e
Tomáš Skýpala <tomas.skypala@kiwi.com>
2021-04-07 11:00:45 +0200

    feat: add faculty and room info data models

---
+++ SmartUniversity/SmartUniversity/Models/Remote/Faculty.swift
@@ -0,0 +1,37 @@
+//
+//  Faculty.swift
+//  SmartUniversity
+//
+//  Created by Tomáš Skýpala on 07.04.2021.
+//  Copyright © 2021 Tomas Skypala. All rights reserved.
+//
+
+struct Faculty: Decodable {
+    let id: String
+    let name: String
+    let rooms: [Room]
+}
+
+struct Room: Decodable {
+
+    let id: String
+
+    let capacity: Int
+    let isLocked: Bool
+
+    let name: String
+    let buildingName: String
+    let roomTypeName: String
+
+    let description: String
+
+    private enum CodingKeys: String, CodingKey {
+        case id = "room_id"
+        case capacity
+        case isLocked = "is_locked"
+        case name
+        case buildingName = "building"
+        case roomTypeName = "type"
+        case description
+    }
+}
